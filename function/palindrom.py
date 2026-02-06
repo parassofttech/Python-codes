6f8c2d8f8213810bd6a9942aa7e788be0b20a9d1
Paras Sahu <parastop5468@gmail.com>
2026-02-06 15:41:10 +0530

    change

--- function/palindrom.py
+++ function/palindrom.py
@@ -1,10 +1,24 @@
-def palindrom(num):
-    rev  = 0
-    while num>0:
-        digit = num % 10
-        rev =rev*10 + digit
-        num = num//10
-    return rev
-num = int(input("Enter your number"))
-palindrom_num = palindrom(num)
-print(palindrom_num)
+# def palindrom(num):
+#     rev  = 0
+#     while num>0:
+#         digit = num % 10
+#         rev =rev*10 + digit
+#         num = num//10
+#     return rev
+# num = int(input("Enter your number"))
+# palindrom_num = palindrom(num)
+# print(palindrom_num)
+
+
+def is_palindrom(text):
+    text = text.lower()
+    rev =""
+    for char in text:
+        rev = char+rev
+    if text == rev:
+        return True
+    else:
+        return False
+text =input("Enter your number")
+palindrom = is_palindrom(text)
+print(palindrom)
